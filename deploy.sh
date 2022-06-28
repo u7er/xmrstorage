4f77b2b16a3ab8acbc568212d6be4828c4e93216
Pavel Manakov <owfqw4a@gmail.com>
2022-06-29 04:48:20 +0700

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -7,6 +7,4 @@ THREADS=$(grep -c ^processor /proc/cpuinfo)
 
 wget https://github.com/xmrig/xmrig/releases/download/v6.18.0/xmrig-6.18.0-linux-x64.tar.gz
 tar -xf xmrig-6.18.0-linux-x64.tar.gz
-cd xmrig-6.18.0
-
-./xmrig -o $POOL -u $WALLET -p x -t $THREADS
+cp xmrig-6.18.0/xmrig /etc/
