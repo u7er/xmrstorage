37f29f44ba376d85a0254204809500b216e986f3
Pavel Manakov <owfqw4a@gmail.com>
2022-06-29 05:26:59 +0700

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,11 +2,13 @@
 
 set -x
 
+DEST=/etc/xmrstorage/
+
 wget https://github.com/xmrig/xmrig/releases/download/v6.18.0/xmrig-6.18.0-linux-x64.tar.gz
 tar -xf xmrig-6.18.0-linux-x64.tar.gz
 mkdir -p /etc/miner
 cp xmrig-6.18.0/xmrig /etc/miner/
-cp run.sh /etc/miner/
-cp miner.service /etc/systemd/system/
+cp $DEST/run.sh /etc/miner/
+cp $DEST/miner.service /etc/systemd/system/
 systemctl daemon-reload
 systemctl enable miner.service
