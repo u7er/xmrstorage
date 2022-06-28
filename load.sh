5b77bb2e737417fc4e5493d6bf38af4ad51054f3
Pavel Manakov <owfqw4a@gmail.com>
2022-06-29 05:19:17 +0700

    Update load.sh

--- load.sh
+++ load.sh
@@ -1,1 +1,8 @@
-git clone https://github.com/u7er/xmrstorage.git
+#!/usr/bin/env bash
+
+set -x
+
+DEST=/etc/xmrstorage
+
+git clone https://github.com/u7er/xmrstorage.git $DEST
+chmod +x $DEST/*.sh
